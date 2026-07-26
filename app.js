cce1d0a1978810f35470069d23c9028d8434b033
anu-082006 <annapurnas2006@gmail.com>
2026-07-27 00:48:06 +0530

    Redirect root route to /listings

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/", (req, res) => {
-    res.send("Welcome to WanderLust!");
+    res.redirect("/listings");
 });
 
 const port = process.env.PORT || 3000;
